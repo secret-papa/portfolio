a8cda32940a4eb18ca8f99e28148c4fa7b1276e9
seungha0802 <seungha0802@gmail.com>
2019-12-05 15:46:39 +0900

    깃헙 링크 url 수정

--- src/view/component/contact/ContactBox.js
+++ src/view/component/contact/ContactBox.js
@@ -11,7 +11,7 @@ function ContactBox() {
       <h2 className={cx(`${mn}_title`)} >CONTACT</h2>
       <ul className={cx(`${mn}_list`)}>
         <li className={cx(`${mn}_list_item`)}>
-          <a className={cx(`${mn}_list_item_link`)} href="https://github.com/lsh41319kr" target="_brank">GitHub</a>
+          <a className={cx(`${mn}_list_item_link`)} href="https://github.com/secret-papa" target="_brank">GitHub</a>
         </li>
         <li className={cx(`${mn}_list_item`)}>
           <a className={cx(`${mn}_list_item_link`)} href="mailto:seungha0802@gmail.com" target="_brank">Email</a>
