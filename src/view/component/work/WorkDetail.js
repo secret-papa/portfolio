21aad0dd3e4dc8b9fcedd931ca0f5c9eebab7631
seungha0802 <seungha0802@gmail.com>
2020-02-03 15:09:55 +0900

    프로덕트 이미지 추가

--- src/view/component/work/WorkDetail.js
+++ src/view/component/work/WorkDetail.js
@@ -9,6 +9,7 @@ const mn = "WorkDetail";
 function WorkDetail({
   desc,
   distribute,
+  media,
   title
 }) {
   return (
@@ -30,22 +31,40 @@ function WorkDetail({
           </ul>
         </div>
       }
+      {
+        !!media.length &&
+        <div className={cx(`${mn}_media`)}>
+          {
+            media.map(({ caption, productEnv, url }) => (
+              <figure key={url} className={cx(`${mn}_media_image`, `${mn}_media_${productEnv}`)}>
+                <img className={cx(`${mn}_media_image_src`)} src={url} alt={'dlalwl'} />
+                <figcaption className={cx(`${mn}_media_image_caption`)}>{caption}</figcaption>
+              </figure>
+            ))
+          }
+        </div>
+      }
     </section>
   )
 }
 
 WorkDetail.defaultProps = {
-  distribute: []
+  distribute: [],
+  media: []
 }
 
 WorkDetail.propTypes = {
   desc: PropTypes.string.isRequired,
   distribute: PropTypes.arrayOf(PropTypes.shape({
     type: PropTypes.string,
+    productEnv: PropTypes.oneOf(['mobile', 'pc']),
     url: PropTypes.string,
   })),
+  media: PropTypes.arrayOf(PropTypes.shape({
+    caption: PropTypes.string,
+    url: PropTypes.string.isRequired
+  })).isRequired,
   title: PropTypes.string.isRequired,
 }
 
 export default WorkDetail
-
