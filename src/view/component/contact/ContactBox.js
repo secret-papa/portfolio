39a249afde993d6aaa92a58023082ab209d44c77
seungha0802 <seungha0802@gmail.com>
2020-02-03 22:27:28 +0900

    블로그 url 추가

--- src/view/component/contact/ContactBox.js
+++ src/view/component/contact/ContactBox.js
@@ -11,6 +11,9 @@ function ContactBox() {
       <h2 className={cx(`${mn}_title`)} >CONTACT</h2>
       <ul className={cx(`${mn}_list`)}>
         <li className={cx(`${mn}_list_item`)}>
+          <a className={cx(`${mn}_list_item_link`)} href="https://secret-papa.github.io/" target="_brank">Blog</a>
+        </li>
+        <li className={cx(`${mn}_list_item`)}>
           <a className={cx(`${mn}_list_item_link`)} href="https://github.com/secret-papa" target="_brank">GitHub</a>
         </li>
         <li className={cx(`${mn}_list_item`)}>
